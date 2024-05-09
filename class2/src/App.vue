fd57c7300d33b0b503c2566146285530061b5b39
yilvquan <129046322+yilvquan@users.noreply.github.com>
2024-05-09 11:25:05 +0800

    Add files via upload

---
+++ class2/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+
+
+  <Navbar @logined="Loginer" @registered="Register"/>
+  <WebHeader @logined="Loginer" @registered="Register" />
+  <LoginPage v-if="canLogin" @Exited="Exiter"/>
+  <RegisterPage v-if="canRegister" @Exited="Exiter"/>
+  
+
+  <Section1 @logined="Loginer" @registered="Register"/>
+  <h1 id="onetitle">课程特色</h1>
+  <Section2 @logined="Loginer" @registered="Register"/>
+  <div class="end">
+    ©2024 云端科技 Artech All rights reserved
+  </div>
+  
+  
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import WebHeader from './components/WebHeader.vue'
+import Section1 from './components/Sectioner1.vue'
+import Section2 from './components/Sectioner2.vue'
+import LoginPage from './components/Login.vue'
+import RegisterPage from './components/Register.vue'
+
+export default {
+  components: {
+    Navbar: Navbar,
+    WebHeader: WebHeader,
+    Section1: Section1,
+    Section2: Section2,
+    LoginPage:LoginPage,
+    RegisterPage:RegisterPage
+
+  },
+  data() {
+    return {
+      canLogin: false,
+      canRegister: false
+    }
+
+
+  },
+  methods:{
+    Loginer(){
+      this.canLogin=true
+      this.canRegister=false
+      console.log(this.canLogin)
+    },
+    Register(){
+      this.canRegister=true
+      this.canLogin=false
+      console.log(this.canRegister)
+    },
+    Exiter(){
+      this.canLogin=false
+      this.canRegister=false
+    }
+  }
+}
+</script>
---
+++ class2/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <nav class="navbar">
+        <img src="./image/屏幕截图 2024-03-29 163318.png" alt="找不到图片">
+        <ul class="log">
+            <li class="lefts" @click="logined"><a>登录</a></li>
+            <li class="rights" @click="registered"><a>注册</a></li>
+
+
+        </ul>
+    </nav>
+
+
+</template>
+<script>
+
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        
+        logined() {
+            this.$emit('logined', "")
+        },
+        registered() {
+            this.$emit('registered', "")
+        }
+    }
+}
+</script>
